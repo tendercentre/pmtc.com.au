0a47d0628de47ecd900fd87e3bb001859e59fe65
Luke Bennett <hello@lukebennett.com.au>
2024-01-31 11:34:07 +1100

    Update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
   output: "export",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
